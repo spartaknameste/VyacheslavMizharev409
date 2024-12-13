741d5d5fa02ff657f9ccfa1859b7043a6250107d
spartaknameste <vaceslavmizarev81@gmail.com>
2024-12-13 17:00:28 +0300

    OOP

---
+++ HomeWork/OOP/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.OOP
+{
+    public record MyDescriptionImmutable
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public MyDescriptionImmutable()
+        {
+            Name = "Вячеслав";
+            SurName = "Мижарёв";
+        }
+    }
+    public record struct MyDescriptionChangeble
+    { 
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public MyDescriptionChangeble(int age, string hobby, int height, int weight)
+        {
+            Age = age;
+            Hobby = hobby;
+            Height = height;
+            Weight = weight;
+        }
+    }
+    public static void Main()
+    {
+        var aboutMe = new MyDescriptionChangeble(19, "Играть в Русы против Ящеров", 184, 76);
+        Console.WriteLine(aboutMe);
+    }
+
+}
